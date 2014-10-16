f7b297a5c99d4c462c690dcc3adf590f358dae80
demouser <codehike@gmail.com>
2014-10-16 05:15:47 -0400

    basic cleanup

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
         _ "fmt"
         "github.com/fsouza/go-dockerclient"
 )
 
+func test(client *docker.Client) {
+	log.Println("Removing container")
+	ContainerId := "abc"
+	if err := client.RemoveContainer(docker.RemoveContainerOptions{}); err != nil {
+		log.Printf("Couldn't remove container %s (%v)\n", ContainerId, err)
+	}
+}
+
 func main() {
         endpoint := "unix:///var/run/docker.sock"
         client, _ := docker.NewClient(endpoint)
-	c, err := client.CreateContainer()
+	test(client)
 }
